d1f849fc66aee4f012ac87b109baa28dbad62fd1
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-24 11:20:14 +0530

    Update extend.py

--- extend.py
+++ extend.py
@@ -1,5 +1,18 @@
 from pytgcalls import PyTgCalls
 from pyrogram import Client
+from typing import List
+from typing import Union
+from pyrogram import filters
+
+COMMAND_PREFIXES = '/'
+
+other_filters = filters.group & ~ filters.edited & \
+    ~ filters.via_bot & ~ filters.forwarded
+other_filters2 = filters.private & ~ filters.edited \
+    & ~ filters.via_bot & ~ filters.forwarded
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
 
 smexy = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 pytgcalls = PyTgCalls(smexy)
