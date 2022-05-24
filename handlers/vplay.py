a3de3b5bb189e9ba1c7f1331a4a4fff63df743ee
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-24 11:05:40 +0530

    Create vplay.py

---
+++ handlers/vplay.py
@@ -0,0 +1,19 @@
+import asyncio
+import os
+
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram import Client as app
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pytgcalls import StreamType
+from pytgcalls.types.input_stream import AudioVideoPiped
+from pytgcalls.types.input_stream.quality import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo,
+)
+from youtubesearchpython import VideosSearch
+
+from config import BOT_NAME, BOT_USERNAME 
