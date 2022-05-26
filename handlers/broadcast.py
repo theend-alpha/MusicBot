fc0958135e164b536cf53830637bf49adc60b789
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-26 22:11:47 +0530

    Update broadcast.py

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -15,17 +15,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`sᴛᴀʀᴛɪɴɢ ʙʀᴏᴀᴅᴄᴀsᴛ​ ʙᴀʙʏ...`")
+        wtf = await message.reply("Starting Broadcast ....")
         if not message.reply_to_message:
-            await wtf.edit("**__ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ​ ʙᴀʙʏ__**")
+            await wtf.edit("**Try: Reply to a message**")
             return
         lmao = message.reply_to_message.text
         async for dialog in Anonymous.iter_dialogs():
             try:
                 await Anonymous.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...` \n\n**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛᴏ :** `{sent}` **ᴄʜᴀᴛs** \n**ꜰᴀɪʟᴇᴅ ɪɴ :** `{failed}` **ᴄʜᴀᴛs**")
+                await wtf.edit(f"`broadcasting...` \n\n**Broadcasted to :** `{sent}` **chats** \n**Failed :** `{failed}` **chats**")
                 await asyncio.sleep(0.3)
             except:
                 failed=failed+1
-        await message.reply_text(f"**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ sᴜᴄᴄᴇssꜰᴜʟʟʏ** \n\n**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛᴏ :** `{sent}` **ᴄʜᴀᴛs** \n**ꜰᴀɪʟᴇᴅ ɪɴ​ :** `{failed}` **ᴄʜᴀᴛs**")
+        await message.reply_text(f"**Broadcasting Done !** \n\n**Broadcasted to :** `{sent}` **chats** \n**Failed​ :** `{failed}` **chats**")
